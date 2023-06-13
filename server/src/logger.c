bb411842009b531b7d045c212f5c1b5f4faf77b4
Gonzalo Martone <gmartone@itba.edu.ar>
2023-06-13 16:09:25 -0300

    fix: make test_parser compile

--- server/src/logger.c
+++ server/src/logger.c
@@ -1,16 +1,16 @@
 #include "logger.h"
 
-LOG_LEVEL current_level = DEBUG;
+LOG_LEVEL current_level = LOGGER_DEBUG;
 
 
 void setLogLevel(LOG_LEVEL newLevel) {
-	if ( newLevel >= DEBUG && newLevel <= FATAL )
+	if ( newLevel >= LOGGER_DEBUG && newLevel <= LOGGER_FATAL )
 	   current_level = newLevel;
 }
 
 char * levelDescription(LOG_LEVEL level) {
     static char * description[] = {"DEBUG", "INFO", "ERROR", "FATAL"};
-    if (level < DEBUG || level > FATAL)
+    if (level < LOGGER_DEBUG || level > LOGGER_FATAL)
         return "";
     return description[level];
 }

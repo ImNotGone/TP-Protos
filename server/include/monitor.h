5988f5b149a7cafd72aacc5f025d6dd0eeb10321
Franco Bosetti <francobosetti@live.com.ar>
2023-06-14 00:57:29 -0300

    chore: monitor is now an ADT

--- server/include/monitor.h
+++ server/include/monitor.h
@@ -1,33 +1,15 @@
 #ifndef SERVER_MONITOR_H
 #define SERVER_MONITOR_H
 
-typedef struct user_log{
-    char * username;
-    char * date_hour;
-    struct user_log * next;
-}user_log_t;
+typedef struct monitorCDT * monitor_t;
 
-typedef user_log_t * user_log_list;
+monitor_t new(unsigned max_users, unsigned max_conns, unsigned queued_conns);
 
-typedef struct metrics{
-    unsigned historic_conns;
-    unsigned current_conns;
-    unsigned bytes_transf;
-    user_log_list first_log; // used for printing
-    user_log_list last_log; // used for insertion
-}metrics_t;
-
-typedef struct config{
-    unsigned max_users;
-    unsigned max_conns;
-    unsigned queued_conns;
-}config_t;
-
-void new_connection(metrics_t * metrics, char * username, char * date_hour);
+void new_connection(monitor_t monitor, char * username, char * date_hour);
 void new_user(char * username);
 
-void set_max_users(unsigned val);
-void set_max_conns(unsigned val);
+void set_max_users(monitor_t monitor, unsigned val);
+void set_max_conns(monitor_t monitor, unsigned val);
 
 void change_password(char * username, char * new_pass);
 
--- server/src/monitor.c
+++ server/src/monitor.c
@@ -1,3 +1,83 @@
 #include <monitor.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct logsCDT{
+    char * username;
+    char * date_hour;
+    struct logsCDT * next;
+}logsCDT;
+
+typedef logsCDT * logs_t;
+
+typedef struct metricsCDT{
+    unsigned historic_conns;
+    unsigned current_conns;
+    unsigned bytes_transf;
+}metricsCDT;
+
+typedef metricsCDT * metrics_t;
+
+typedef struct configCDT{
+    unsigned max_users;
+    unsigned max_conns;
+    unsigned queued_conns;
+}configCDT;
+
+typedef configCDT * config_t;
+
+typedef struct monitorCDT{
+    metrics_t metrics;
+    config_t config;
+    logs_t first_log; // used for printing
+    logs_t last_log; // used for insertion
+}monitorCDT;
+
+//TODO: check if parameters are zero
+monitor_t new(unsigned max_users, unsigned max_conns, unsigned queued_conns){
+    monitor_t new = calloc(1, sizeof(monitorCDT));
+
+    metrics_t metrics = calloc(1, sizeof(metricsCDT));
+    config_t config = malloc(sizeof(configCDT));
+
+    config->max_users=max_users;
+    config->max_conns=max_conns;
+    config->queued_conns=queued_conns;
+
+    new->metrics=metrics;
+    new->config=config;
+
+    return new;
+}
+
+//TODO: check nulls
+void new_connection(monitor_t monitor, char * username, char * date_hour){
+    monitor->metrics->historic_conns++;
+    monitor->metrics->current_conns++;
+    logs_t new_node = malloc(sizeof(logsCDT));
+    new_node->username = username;
+    new_node->date_hour = date_hour;
+    new_node->next = NULL;
+    monitor->last_log->next = new_node;
+    monitor->last_log  = new_node;
+}
+
+void new_user(char * username){
+
+}
+
+void set_max_users(monitor_t monitor, unsigned val){
+    monitor->config->max_users=val;
+}
+
+void set_max_conns(monitor_t monitor, unsigned val){
+    monitor->config->max_conns=val;
+}
+
+void change_password(char * username, char * new_pass){
+
+}
+
+void delete_user(char * username){
+
+}

8022948b6e717bca90de73f8b24355347b6dec4e
Juan Cruz Bafico <jbafico@itba.edu.ar>
2023-06-19 18:29:41 -0300

    chore: added transaction functions

--- server/include/states/transaction.h
+++ server/include/states/transaction.h
@@ -4,6 +4,8 @@
 #include <selector.h>
 #include <pop3.h>
 
+#define BLOQUE 10
+
 states_t transaction_read(struct selector_key * key);
 states_t transaction_write(struct selector_key * key);
 
--- server/src/states/transaction.c
+++ server/src/states/transaction.c
@@ -2,17 +2,21 @@
 #include <commands.h>
 #include <states/transaction.h>
 #include <states/states-common.h>
+#include <message-manager.h>
+#include <string.h>
+#include <stdio.h>
 #include <responses.h>
 #include <assert.h>
 
 static states_t handle_stat(client_t * client_data, char * unused1, int unused2 , char * unused3, int unused4);
 static states_t handle_list(client_t * client_data, char * unused1, int unused2 , char * unused3, int unused4);
 static states_t handle_retr(client_t * client_data, char * unused1, int unused2 , char * unused3, int unused4);
-static states_t handle_dele(client_t * client_data, char * unused1, int unused2 , char * unused3, int unused4);
+static states_t handle_dele(client_t * client_data, char * message_number, int unused1 , char * unused2, int unused3);
 static states_t handle_noop(client_t * client_data, char * unused1, int unused2 , char * unused3, int unused4);
 static states_t handle_rset(client_t * client_data, char * unused1, int unused2 , char * unused3, int unused4);
 static states_t handle_capa(client_t * client_data, char * unused1, int unused2 , char * unused3, int unused4);
 static states_t handle_quit(client_t * client_data, char * unused1, int unused2 , char * unused3, int unused4);
+static char * concat(char * dir_ini, size_t pos, const char * source, size_t * dim);
 
 static command_t commands[] = {
     { .name = "stat", .command_handler = handle_stat},
@@ -37,27 +41,59 @@ states_t transaction_write(struct selector_key * key) {
 }
 
 static states_t handle_stat(client_t * client_data, char * unused1, int unused2 , char * unused3, int unused4) {
-    assert(0 && "Unimplemented");
+    client_data->response_index=0;
+    int message_count, message_size;
+    size_t response_size=0;
+    message_manager_get_maildrop_info(client_data->message_manager, &message_count, &message_size);
+    client_data->response_is_mallocced=true;
+
+    char * response= malloc(512);
+    char message_string_count[3];//TODO ver tema magic numbers
+    char message_string_size[4];
+
+    sprintf(message_string_count, "%d", message_count);
+    sprintf(message_string_size, "%d", message_size);
+
+    response= concat(response, response_size, OK_HEADER, &response_size);
+    response= concat(response, response_size, message_string_count, &response_size);
+    response= concat(response, response_size, message_string_size, &response_size);
+
+    client_data->response=response;
+    states_common_response_write(&client_data->buffer_out, client_data->response, &client_data->response_index);
+    return TRANSACTION;
 }
 static states_t handle_list(client_t * client_data, char * unused1, int unused2 , char * unused3, int unused4) {
-    assert(0 && "Unimplemented");
+    client_data->response_index=0;
+    return TRANSACTION;
 }
 static states_t handle_retr(client_t * client_data, char * unused1, int unused2 , char * unused3, int unused4) {
     assert(0 && "Unimplemented");
 }
-static states_t handle_dele(client_t * client_data, char * unused1, int unused2 , char * unused3, int unused4) {
-    assert(0 && "Unimplemented");
+static states_t handle_dele(client_t * client_data, char * message_number, int unused1 , char * unused2, int unused3) {
+    client_data->response_index=0;
+    client_data->response=RESPONSE_TRANSACTION_DELE_ERROR;
+    if(message_manager_delete_message(client_data->message_manager, atoi(message_number))==MESSAGE_SUCCESS){
+        client_data->response=RESPONSE_TRANSACTION_DELE_SUCCESS;
+    }
+    client_data->response_is_mallocced=false;
+    states_common_response_write(&client_data->buffer_out, client_data->response, &client_data->response_index);
+    return TRANSACTION;
 }
 static states_t handle_noop(client_t * client_data, char * unused1, int unused2 , char * unused3, int unused4) {
     client_data->response_index=0;
     client_data->response=RESPONSE_TRANSACTION_NOOP;
+    client_data->response_is_mallocced=false;
     states_common_response_write(&client_data->buffer_out, client_data->response, &client_data->response_index);
     return TRANSACTION;
 }
 static states_t handle_rset(client_t * client_data, char * unused1, int unused2 , char * unused3, int unused4) {
-    //TODO unmark deleted emails when ADT is ready
+    if(message_manager_reset_deleted_flag(client_data->message_manager)!=MESSAGE_SUCCESS){
+        //TODO log error
+        return ERROR;
+    }
     client_data->response_index=0;
     //TODO RFC says possible outcome +OK but in example there is more text on the server response to the client
+    client_data->response_is_mallocced=false;
     client_data->response=RESPONSE_TRANSACTION_RSET;
     states_common_response_write(&client_data->buffer_out, client_data->response, &client_data->response_index);
     return TRANSACTION;
@@ -66,6 +102,7 @@ static states_t handle_rset(client_t * client_data, char * unused1, int unused2 
 
 static states_t handle_capa(client_t * client_data, char * unused1, int unused2 , char * unused3, int unused4) {
     client_data->response_index = 0;
+    client_data->response_is_mallocced=false;
     client_data->response = RESPONSE_TRANSACTION_CAPA;
     states_common_response_write(&client_data->buffer_out, client_data->response, &client_data->response_index);
     return TRANSACTION;
@@ -74,5 +111,18 @@ static states_t handle_capa(client_t * client_data, char * unused1, int unused2 
 static states_t handle_quit(client_t * client_data, char * unused1, int unused2 , char * unused3, int unused4) {
     // Go to update state for it to be handled there
     return UPDATE;
+}
+
+static char * concat(char * dir_ini, size_t pos, const char * source, size_t * dim) {
+    int i;
+    for(i = 0; source[i] != 0; i++) {
+        if(i % BLOQUE == 0)
+            dir_ini = realloc(dir_ini, (pos+i+BLOQUE)* sizeof(char));
+        dir_ini[pos+i] = source[i];
+    }
+    dir_ini = realloc(dir_ini, (pos+i+1)*sizeof(char));
+    dir_ini[pos+i] = '\0';
+    *dim = pos+i;
+    return dir_ini;
 }
 

d04ea003088ce9a65ebef80da00205d4ee8695ca
Juan Cruz Bafico <jbafico@itba.edu.ar>
2023-06-23 11:51:45 -0300

    chore: created folder for monitor

---
+++ monitor/include/client_utils.h
@@ -0,0 +1,9 @@
+//
+// Created by juanc on 6/23/2023.
+//
+
+#ifndef TP_PROTOS_CLIENT_UTILS_H
+#define TP_PROTOS_CLIENT_UTILS_H
+
+#endif //TP_PROTOS_CLIENT_UTILS_H
+
---
+++ monitor/src/client_utils.c
@@ -0,0 +1,5 @@
+//
+// Created by juanc on 6/23/2023.
+//
+
+#include "../include/client_utils.h"
